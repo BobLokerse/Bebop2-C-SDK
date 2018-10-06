69e3235df1812ada5aa8103c152765de7f25fa46
Bob Lokerse <lokerse@hotmail.com>
2018-10-06 10:03:05 +0200

    Extracted method ExecuteCommandOnUserInput

--- StandalonePC/drone_UDP/drone_UDP/Program.cs
+++ StandalonePC/drone_UDP/drone_UDP/Program.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace drone_UDP
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
             //This is a sample about using the pilotting command.
@@ -15,68 +15,73 @@ namespace drone_UDP
                 Console.ReadLine();
                 return;
             }
-            else
+
+            while (true)
             {
-                while (true)
-                {
-                    string input = Console.ReadLine();
-                    switch (input)
-                    {
-                        //takeoff
-                        case "t":
-                            bebop.takeoff();
-                            break;
-                        //landing
-                        //moving command: -100% ~ 100%
-                        case "l":
-                            bebop.landing();
-                            break;
-                        //left
-                        case "a":
-                            bebop.move(1, -10, 0, 0, 0);
-                            break;
-                        //right
-                        case "d":
-                            bebop.move(1, 10, 0, 0, 0);
-                            break;
-                        //forward
-                        case "w":
-                            bebop.move(1, 0, 10, 0, 0);
-                            break;
-                        //backward
-                        case "s":
-                            bebop.move(1, 0, -10, 0, 0);
-                            break;
-                        //turn left
-                        case "h":
-                            bebop.move(0, 0, 0, -10, 0);
-                            break;
-                        //turn right
-                        case "k":
-                            bebop.move(0, 0, 0, 10, 0);
-                            break;
-                        //up
-                        case "u":
-                            bebop.move(0, 0, 0, 0, 10);
-                            break;
-                        //down
-                        case "j":
-                            bebop.move(0, 0, 0, 0, -10);
-                            break;
-                        //pause
-                        case "p":
-                            bebop.move(0, 0, 0, 0, 0);
-                            break;
-                        case "v":
-                            bebop.videoEnable(); //enable RTP/.H264 videostreaming
-                            break;
-                        //quit
-                        case "q":
-                            bebop.cancleAllTask();
-                            return;
-                    }
-                }
+                if (ExecuteCommandOnUserInput(bebop)) return;
             }
+        }
+
+        private static bool ExecuteCommandOnUserInput(BebopCommand bebop)
+        {
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                //takeoff
+                case "t":
+                    bebop.takeoff();
+                    break;
+                //landing
+                //moving command: -100% ~ 100%
+                case "l":
+                    bebop.landing();
+                    break;
+                //left
+                case "a":
+                    bebop.move(1, -10, 0, 0, 0);
+                    break;
+                //right
+                case "d":
+                    bebop.move(1, 10, 0, 0, 0);
+                    break;
+                //forward
+                case "w":
+                    bebop.move(1, 0, 10, 0, 0);
+                    break;
+                //backward
+                case "s":
+                    bebop.move(1, 0, -10, 0, 0);
+                    break;
+                //turn left
+                case "h":
+                    bebop.move(0, 0, 0, -10, 0);
+                    break;
+                //turn right
+                case "k":
+                    bebop.move(0, 0, 0, 10, 0);
+                    break;
+                //up
+                case "u":
+                    bebop.move(0, 0, 0, 0, 10);
+                    break;
+                //down
+                case "j":
+                    bebop.move(0, 0, 0, 0, -10);
+                    break;
+                //pause
+                case "p":
+                    bebop.move(0, 0, 0, 0, 0);
+                    break;
+                case "v":
+                    bebop.videoEnable(); //enable RTP/.H264 videostreaming
+                    break;
+                //quit
+                case "q":
+                    bebop.cancleAllTask();
+                    return true;
+            }
+
+            return false;
         }
     }
 }
